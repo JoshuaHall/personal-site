0baf89104133a1e78fb550475c3fd4288c21051f
JoshuaHall <joshuahallmail@gmail.com>
2020-05-05 02:18:36 -0500

    Removed stylelint from postcss config, wasn't working how I wanted

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const stylelint = require('stylelint');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 
 module.exports = {
   plugins: [
-    stylelint(),
     autoprefixer(),
     cssnano({
       preset: 'default',
