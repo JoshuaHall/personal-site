dd0371643bda8aa3a6b75a23e116709546085e90
JoshuaHall <joshuahallmail@gmail.com>
2020-05-11 17:24:43 -0500

    Added jsx-a11y eslint rules/plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,11 +15,12 @@ module.exports = {
   },
   extends: [
     'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'plugin:jsx-a11y/recommended', // Uses the recommended rules from eslint-plugin-jsx-a11y
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
-  plugins: ['react-hooks'],
+  plugins: ['react-hooks', 'jsx-a11y'],
   ignorePatterns: ['public/'],
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
